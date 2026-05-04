ac9e326981d74d48058045a5392c6199d905fd3a
Shadow-hunter58 <visheshpoojary29@gmail.com>
2026-05-04 23:49:31 +0530

    new file

--- B8.cpp
+++ B8.cpp
@@ -1,62 +1,42 @@
-// devolop a c++ program to demonstrate multiple inheritance by implementing two base classess alpha and beta each containing a protected data member and a constructor to intialize it. create a derived class gamma that inherits from both alpha and  beta with a constructor that initializes all the data members and a member function to display the values of base class members along with any additional members of gamma. in the main function. create  an object of gamma and use its member funnctions to display all the values of data members.
-
-#include <iostream>
-using namespace std;
-
-class alpha 
-{
-protected:
-    int a;
-public:
-    alpha(int val) 
-    {
-    	a=val;
-        cout << "Alpha is initialized." << endl;
-    }  
-};
-
-class beta 
-{
-protected:
-    int b;
-public:
-    beta(int val) 
-    {	2. Basic + Bonus
-3. Basic + Bonus + Overtime
-0. Exit
-
-    	b=val;
-        cout << "Beta is initialized." << endl;
-    }
-};
-
-class gamma : public alpha, public beta 
-{
-private:
-    int g;
-public:
-	gamma(int v1, int v2, int v3) : alpha(v1), beta(v2)
-	{
-		g=v3;
-        cout << "Gamma is initialized." << endl;
-    }
-    void display() 
-    {
-        cout << "\nValue from Alpha : " << a << endl;
-        cout << "Value from Beta  : " << b << endl;
-        cout << "Value from Gamma : " << g << endl;
-    }
-};
-
-int main()
-{
-	gamma g(140,2045,3450);
-	g.display();
-
-return 0;
-}
-
-
-
-
-
+#include<iostream> 
+using namespace std; 
+class Alpha 
+{ 
+    protected: int x; 
+    public: 
+    Alpha(int x) 
+    { 
+        this->x=x; 
+    } 
+}; 
+class Beta 
+{ 
+    protected: int y ; 
+    public: 
+    Beta(int y) 
+    { 
+        this->y=y; 
+    } 
+}; 
+class Gamma:Alpha,Beta 
+{ 
+    private: int z; 
+    public: 
+    Gamma(int x,int y,int z):Alpha(x),Beta(y) 
+    { 
+        this->z=z; 
+    } 
+     void display() 
+    { 
+        cout<<"Value of x from Alpha: "<<x<<endl; 
+        cout<<"Value of y from Beta: "<<y<<endl; 
+        cout<<"Value of z from Gamma: "<<z<<endl; 
+    } 
+}; 
+ 
+int main() 
+{ 
+    Gamma g(10,20,30); 
+    g.display(); 
+    return 0; 
+} 
